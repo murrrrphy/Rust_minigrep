634ad7509b5de7e3791701b702081d968c9ec3af
qyc <987127314@qq.com>
2021-07-22 15:46:03 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents){
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename})
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut result = Vec::new();
+
+    for lines in contents.lines() {
+        if lines.contains(query) {
+            result.push(lines);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+}
